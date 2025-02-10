a7620beb5851d1dc586d9716802e281aaa950303
not2and <andre.c.felix@aluno.senai.br>
2025-02-10 15:46:51 -0300

    Codigo com mais itens

--- TesteVeiculo.java
+++ TesteVeiculo.java
@@ -2,6 +2,7 @@ public class TesteVeiculo {
     public static void main(String[] args) {
         //criacao de um objeto da classe Veiculo
         Veiculo celtarebaixado = new Veiculo();
+        Veiculo corsarebaixado = new Veiculo();
         //atribuicao de valores aos atributos do objeto
 
         celtarebaixado.marca = "Chevrolet";
@@ -23,5 +24,26 @@ public class TesteVeiculo {
 
         System.out.println("\n\t\t\t -- Meu Celta Parado -- \n");
         celtarebaixado.exibirVeiculo();
+
+        corsarebaixado.marca = "Chevrolet";
+        corsarebaixado.modelo = "Corsa";
+        corsarebaixado.cor = 'p';
+        corsarebaixado.velocidade = 0;
+        System.out.println("\n\t\t\t -- Meu Corsa -- \n");
+        corsarebaixado.exibirVeiculo();
+
+        //acelerando o veiculo
+        for (int i = 0; i < 120; i++)
+            corsarebaixado.acelerar();
+
+        System.out.println("\n\t\t\t -- Meu Corsa No Autodromo -- \n");
+        corsarebaixado.exibirVeiculo();
+
+        for (int i = 120; i > 0; i--)
+            corsarebaixado.desacelerar();
+
+        System.out.println("\n\t\t\t -- Meu Corsa Parado -- \n");
+        corsarebaixado.exibirVeiculo();
+
     }
 }
